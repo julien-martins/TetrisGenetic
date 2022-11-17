beaa4fcf38d871fd41ab008ca007ed534042b964
julien-martins <julien.martins-almoster@hotmail.com>
2022-11-17 13:55:15 +0100

    Add asset and script board

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,12 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class Board : MonoBehaviour
 {
+    public TileBase[] Tiles;
+
+    private Tilemap _tilemap;
+
+    private Piece piece;
+
     // Start is called before the first frame update
     void Start()
     {
+        _tilemap = GetComponentInChildren<Tilemap>();
+        _tilemap.SetTile(new Vector3Int(0, 0, 0), Tiles[0]);
+        
         
     }
 
--- Assets/Scripts/Piece.cs
+++ Assets/Scripts/Piece.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Piece : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    enum TETRO_TYPE
     {
-        
+        L,
+        I,
+        O,
+        S,
+        Z,
+        T
     }
 
-    // Update is called once per frame
-    void Update()
+    private int[,] tiles = new int[4, 4];
+    private TETRO_TYPE type;
+    
+    public Piece()
     {
-        
+        type = TETRO_TYPE.L;
+        generatePiece();
     }
+
+    private void generatePiece()
+    {
+        switch (type)
+        {
+            case TETRO_TYPE.L:
+                tiles[0, 0] = 0; tiles[1, 0] = 0; tiles[2, 0] = 0; tiles[3, 0] = 0;
+                tiles[0, 1] = 0; tiles[1, 1] = 0; tiles[2, 1] = 0; tiles[3, 1] = 0;
+                tiles[0, 2] = 0; tiles[1, 2] = 0; tiles[2, 2] = 0; tiles[3, 2] = 0;
+                tiles[0, 3] = 0; tiles[1, 3] = 0; tiles[2, 3] = 0; tiles[3, 3] = 0;
+                
+                break;
+            default:
+                break;
+        }
+    }
+    
 }
